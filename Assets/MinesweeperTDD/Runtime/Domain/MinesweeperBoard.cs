e2f06f0c6eab9f97ea506044af9ea30983b7f198
RGV <ricardo.guzman.velasco@outlook.com>
2021-04-24 22:05:25 +0200

    BombsCount (dummy)

--- Assets/MinesweeperTDD/Runtime/Domain/MinesweeperBoard.cs
+++ Assets/MinesweeperTDD/Runtime/Domain/MinesweeperBoard.cs
@@ -10,11 +10,34 @@ namespace MinesweeperTDD.Tests
 {
     public class MinesweeperBoard : Board
     {
-        public MinesweeperBoard(int sizeX, int sizeY) : base(sizeX, sizeY)
+        int bombCount; 
+        
+        public MinesweeperBoard(int sizeX, int sizeY, int bombCount) : base(sizeX, sizeY)
         {
-            for (var i = 0; i < sizeX; i++)
-                for (var j = 0; j < sizeY; j++)
-                    tiles[(i, j)].Content = new MinesweeperContent(false);
+            AssertBombCountIsPositive(bombCount);
+            
+            this.bombCount = bombCount;
+            
+            InitTileContents(sizeX, sizeY);
+            InitBombs(bombCount);
+        }
+
+        void InitTileContents(int sizeX, int sizeY)
+        {
+            for(var i = 0; i < sizeX; i++)
+            for(var j = 0; j < sizeY; j++)
+                tiles[(i, j)].Content = new MinesweeperContent(false);
+        }
+
+        void InitBombs(int bombsToSpawn)
+        {
+            tiles[(0, 0)].Content = new MinesweeperContent(true);
+        }
+
+        static void AssertBombCountIsPositive(int bombCount)
+        {
+            if(bombCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(bombCount));
         }
     }
 }
--- Assets/MinesweeperTDD/Tests/Editor/MinesweeperBoardTests.cs
+++ Assets/MinesweeperTDD/Tests/Editor/MinesweeperBoardTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Kalendra.BoardSystem.Runtime.Domain.Entities;
 using MinesweeperTDD.Runtime.Domain;
 using NUnit.Framework;
@@ -11,18 +12,37 @@ namespace MinesweeperTDD.Tests
         [Test]
         public void Size_IsSet_ByConstructor()
         {
-            var sut = new MinesweeperBoard(3, 4);
+            var sut = new MinesweeperBoard(3, 4, 1);
 
             var result = sut.Size;
 
             result.Should().Be((3, 4));
         }
+
+        [Test]
+        public void BombCount_NonPositive_ThrowsException()
+        {
+            Action act = () =>  new MinesweeperBoard(1, 1, 0);
+
+            act.Should().ThrowExactly<ArgumentOutOfRangeException>();
+        }
+        
+        [Test]
+        public void BombCount_IsSet_ByConstructor()
+        {
+            var sut = new MinesweeperBoard(1, 1, 1);
+            
+            var result = sut[0, 0].Content as MinesweeperContent;
+            
+            result.IsBomb.Should().BeTrue();
+        }
+        
         [Test]
         public void EveryTile_Has_MinesweeperContent()
         {
-            var sut = new MinesweeperBoard(1, 1);
+            var sut = new MinesweeperBoard(1, 1, 1);
 
-            var result = sut.GetTile(0, 0).Content;
+            var result = sut[0, 0].Content;
 
             result.Should().BeOfType<MinesweeperContent>();
         }
